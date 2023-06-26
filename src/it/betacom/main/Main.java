8f2f066f6459dc1afbb29ea4490d28a75fc2f915
Daniele <ildani@email.it>
2023-06-26 22:28:30 +0200

    4

--- src/it/betacom/main/Main.java
+++ src/it/betacom/main/Main.java
@@ -3,7 +3,7 @@ package it.betacom.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Avvioccccc");
+		System.out.println("Avviocccc44444444444444c");
         
 	}
 
